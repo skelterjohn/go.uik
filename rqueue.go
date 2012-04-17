56994f58958100e050f40bac0e250d097f6cfc22
John Asmuth <jasmuth@gmail.com>
2012-04-16 23:02:04 -0400

    stealing kevlar's iq

--- rqueue.go
+++ rqueue.go
@@ -1,85 +1,77 @@
 package uik
 
-type RB struct {
-	length, start int
+// this code stolen from github.com/kylelemons/iq
+
+type Ring struct {
+	cnt, i int
 	data []interface{}
 }
 
-func (rb *RB) Enqueue(x interface{}) {
-	if rb.length >= len(rb.data) {
-		rb.sizeUp(2*rb.length+1)
-	}
-	index := rb.start + rb.length
-	index = index % len(rb.data)
-	rb.data[index] = x
-	rb.length++
+func (rb *Ring) Empty() bool {
+	return rb.cnt == 0
 }
 
-func (rb *RB) Dequeue() (x interface{}) {
-	x = rb.data[rb.start]
-	rb.start = (rb.start + 1) % len(rb.data)
-	rb.length--
-	
-	if rb.start == 0 {
-		rb.data = rb.data[:rb.length]
-	}
-	
-	return
+func (rb *Ring) Peek() interface{} {
+	return rb.data[rb.i]
 }
 
-func (rb *RB) sizeUp(newSize int) {
+func (rb *Ring) Enqueue(x interface{}) {
+	if rb.cnt >= len(rb.data) {
+		rb.grow(2 * rb.cnt + 1)
+	}
+	rb.data[(rb.i + rb.cnt) % len(rb.data)] = x
+	rb.cnt++
+}
+
+func (rb *Ring) Dequeue() {
+	rb.cnt, rb.i = rb.cnt - 1, (rb.i + 1) % len(rb.data)
+}
+
+func (rb *Ring) grow(newSize int) {
 	newData := make([]interface{}, newSize)
-	//copy the stuff at the end
-	endLength := len(rb.data) - rb.start
-	if endLength > rb.length {
-		endLength = rb.length
-	}
-	copy(newData[:endLength], rb.data[rb.start:rb.start+endLength])
-	
-	// did it wrap?
-	if endLength < rb.length {
-		//copy the stuff at the beginning
-		endIndex := (rb.start + rb.length) % len(rb.data)
-		copy(newData[rb.start+endLength:rb.start+rb.length], rb.data[:endIndex])
-	}
-	rb.start = 0
+
+	n := copy(newData, rb.data[rb.i:])
+	copy(newData[n:], rb.data[:rb.cnt-n])
+
+	rb.i = 0
 	rb.data = newData
 }
 
+func RingIQ(in <-chan interface{}, next chan<- interface{}) {
+	var rb Ring
+	defer func() {
+		for !rb.Empty() {
+			next <- rb.Peek()
+			rb.Dequeue()
+		}
+		close(next)
+	}()
+
+	for {
+		if rb.Empty() {
+			v, ok := <-in
+			if !ok {
+				return
+			}
+			rb.Enqueue(v)
+		}
+
+		select {
+		case next <- rb.Peek():
+			rb.Dequeue()
+		case v, ok := <-in:
+			if !ok {
+				return
+			}
+			rb.Enqueue(v)
+		}
+	}
+}
 func QueuePipe() (in chan<- interface{}, out <-chan interface{}) {
 	inch := make(chan interface{})
 	in = inch
 	outch := make(chan interface{})
 	out = outch
-	go func(inch <-chan interface{}, outch chan<- interface{}) {
-		var rb RB
-		var cur interface{}
-		for {
-			if rb.length == 0 {
-				var ok bool
-				cur, ok = <-inch
-				if !ok {
-					close(outch)
-					return
-				}
-			}
-			select {
-			case outch<- cur:
-				if rb.length != 0 {
-					cur = rb.Dequeue()
-				}
-			case v, ok := <-inch:
-				if !ok {
-					outch<- cur
-					for rb.length != 0 {
-						outch<- rb.Dequeue()
-					}
-					close(outch)
-					return
-				}
-				rb.Enqueue(v)
-			}
-		}
-	}(inch, outch)
+	go RingIQ(inch, outch)
 	return
 }
