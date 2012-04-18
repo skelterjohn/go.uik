9e0b4ee9b2990cc961efd12e817d9cbed5a6663e
John Asmuth <jasmuth@gmail.com>
2012-04-17 20:39:14 -0400

    text pre-renderer

--- fonts.go
+++ fonts.go
@@ -1,6 +1,8 @@
 package uik
 
 import (
+	"image"
+	"image/color"
 	"code.google.com/p/freetype-go/freetype/truetype"
 	"code.google.com/p/draw2d/draw2d"
 )
@@ -9,6 +11,37 @@ var DefaultFontData = draw2d.FontData {
 	Name: "luxi",
 	Family: draw2d.FontFamilySans,
 	Style: draw2d.FontStyleNormal,
+}
+
+func GetFontHeight(fd draw2d.FontData, size float64) (height float64) {
+	font := draw2d.GetFont(fd)
+	bounds := font.Bounds()
+	height = float64(bounds.YMax - bounds.YMin)*size/float64(font.UnitsPerEm())
+	return
+}
+
+func RenderString(text string, fd draw2d.FontData, size float64, color color.Color) (buffer image.Image) {
+	height := GetFontHeight(fd, size)
+	widthMax := float64(len(text)) * size
+	
+	buf := image.NewRGBA(image.Rectangle{
+		Min: image.Point{0, 0},
+		Max: image.Point{int(widthMax+1), int(height+1)},
+	})
+
+	gc := draw2d.NewGraphicContext(buf)
+	gc.Translate(0, height)
+	gc.SetFontData(fd)
+	gc.SetFontSize(size)
+	gc.SetStrokeColor(color)
+	width := gc.FillString(text)
+
+	buffer = buf.SubImage(image.Rectangle{
+		Min: image.Point{0, 0},
+		Max: image.Point{int(width+1), int(height+1)},
+	})
+
+	return
 }
 
 func init() {
--- label.go
+++ label.go
@@ -2,6 +2,7 @@ package uik
 
 import (
 	"code.google.com/p/draw2d/draw2d"
+	"image"
 	"image/color"
 )
 
@@ -13,6 +14,8 @@ type Label struct {
 
 	FontSize float64
 	FontSizeCh chan float64
+
+	tbuf image.Image
 }
 
 func NewLabel(size Coord, text string) (l *Label) {
@@ -39,28 +42,33 @@ func NewLabel(size Coord, text string) (l *Label) {
 func (l *Label) draw(gc draw2d.GraphicContext) {
 	gc.SetStrokeColor(color.Black)
 	
-	font := draw2d.GetFont(gc.GetFontData())
-	bounds := font.Bounds()
-	height := float64(bounds.YMax - bounds.YMin)*l.FontSize/float64(font.UnitsPerEm())
+	// height := GetFontHeight(gc.GetFontData(), l.FontSize)
 	
-	offset := l.Size.Y - (l.Size.Y - height) / 2
+	// offset := l.Size.Y - (l.Size.Y - height) / 2
 
 	safeRect(gc, Coord{0, 0}, l.Size)
 	gc.FillStroke()
-	gc.Translate(10, offset)
-	gc.SetFontData(DefaultFontData)
-	gc.SetFontSize(l.FontSize)
-	gc.FillString(l.Text)
+	tw := float64(l.tbuf.Bounds().Max.X -  l.tbuf.Bounds().Min.X)
+	th := float64(l.tbuf.Bounds().Max.Y -  l.tbuf.Bounds().Min.Y)
+	gc.Translate((l.Size.X-tw)/2, (l.Size.Y-th)/2)
+	gc.DrawImage(l.tbuf)
+
+	// gc.Translate(10, offset)
+	// gc.SetFontData(DefaultFontData)
+	// gc.SetFontSize(l.FontSize)
+	// gc.FillString(l.Text)
 }
 
 func (l *Label) handleEvents() {
 	for {
 		select {
 		case l.Text = <-l.TextCh:
+			l.tbuf = RenderString(l.Text, DefaultFontData, l.FontSize, color.Black)
 			if l.Parent != nil {
 				l.RedrawIn <- RedrawEvent{l.BoundsInParent()}
 			}
 		case l.FontSize = <-l.FontSizeCh:
+			l.tbuf = RenderString(l.Text, DefaultFontData, l.FontSize, color.Black)
 			if l.Parent != nil {
 				l.RedrawIn <- RedrawEvent{l.BoundsInParent()}
 			}
