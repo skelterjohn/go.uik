f6df1631d57c9afed0a592bb2954a89c97d9dd60
John Asmuth <jasmuth@gmail.com>
2012-05-12 17:47:41 -0400

    i mean uik, not wde

--- wfound.go
+++ wfound.go
@@ -1,5 +1,5 @@
 /*
-   Copyright 2012 the go.wde authors
+   Copyright 2012 the go.uik authors
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
